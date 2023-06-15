eae96bd742368c10c4701249bd4ada958b3982ce
Frolova199128 <125869363+Frolova199128@users.noreply.github.com>
2023-06-15 07:41:19 +0300

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,16 @@
+# Задача 26 
+# def power(a, b):
+#    if b == 0:
+#       return 1
+#    return a * power(a, b - 1)
+# a = int(input('Введите число: '))
+# b = int(input('Введите степень числа: '))
+# print(power(a, b))
+
+# Задача 28
+# def num(n):
+#    if n == 0 or n == 1:
+#        return f'{n}'
+#    return num(n // 2) + f'{n % 2}'
+# n = int(input('Введите число: '))
+# print(num(n))
